37f21861a86adcd966e749e37db39310f5e57f4d
Mateusz Sobkowiak <60814050+Sobgo@users.noreply.github.com>
2024-04-14 21:02:32 +0200

    Added rest of the tests

--- test/convex_hull_test.cpp
+++ test/convex_hull_test.cpp
@@ -1,6 +1,9 @@
 #include "../src/convex_hull_lib/convex_hull.hpp"
 #include <algorithm>
 #include <catch2/catch_test_macros.hpp>
+#include <ctime>
+#include <random>
+#include <set>
 #include <vector>
 using namespace std;
 
@@ -18,22 +21,122 @@ SCENARIO("Convex Hull is calculated correctly") {
 			vector<Point> hull = convex_hull(in);
 
 			THEN("It is a subset of the input") {
-				REQUIRE(is_subset(hull, in));
+				REQUIRE(is_subset(in, hull));
 			}
-
-			THEN("It has at least 3 points") { REQUIRE(hull.size() >= 3); }
 
 			THEN("It is convex") { REQUIRE(is_convex(hull)); }
 
 			THEN("All input points are inside the hull") {
-				REQUIRE(all_inside_polygon(in, hull));
+				REQUIRE(all_inside_polygon(hull, in));
 			}
 		}
 	}
 
-	// TODO edge cases
+	GIVEN("Empty set") {
+		vector<Point> in;
 
-	// TODO randomized tests
+		WHEN("Convex Hull is calculated") {
+			vector<Point> hull = convex_hull(in);
+
+			THEN("It is empty") { REQUIRE(hull.empty()); }
+		}
+	}
+
+	GIVEN("A set with one point") {
+		vector<Point> in = {{7.3, 8.23}};
+
+		WHEN("Convex Hull is calculated") {
+			vector<Point> hull = convex_hull(in);
+
+			THEN("It is the same point") { REQUIRE(hull == in); }
+		}
+	}
+
+	GIVEN("A set of 2 points") {
+		vector<Point> in = {{3.14, 2.71}, {1.61, 9.81}};
+
+		WHEN("Convex Hull is calculated") {
+			vector<Point> hull = convex_hull(in);
+
+			THEN("It is the same set") {
+				REQUIRE(hull.size() == in.size());
+				REQUIRE(is_subset(hull, in));
+			}
+		}
+	}
+
+	GIVEN("A set of 3 collinear points") {
+		vector<Point> in = {{1, 1}, {2, 2}, {3, 3}};
+
+		WHEN("Convex Hull is calculated") {
+			vector<Point> hull = convex_hull(in);
+
+			THEN("It is the same set") {
+				REQUIRE(hull.size() == in.size());
+				REQUIRE(is_subset(hull, in));
+			}
+		}
+	}
+
+	GIVEN("A set of 3 points on vertical line") {
+		vector<Point> in = {{1, 1}, {1, 2}, {1, 3}};
+
+		WHEN("Convex Hull is calculated") {
+			vector<Point> hull = convex_hull(in);
+
+			THEN("It is the same set") {
+				REQUIRE(hull.size() == in.size());
+				REQUIRE(is_subset(hull, in));
+			}
+		}
+	}
+
+	GIVEN("A set of 3 points on horizontal line") {
+		vector<Point> in = {{1, 1}, {2, 1}, {3, 1}};
+
+		WHEN("Convex Hull is calculated") {
+			vector<Point> hull = convex_hull(in);
+
+			THEN("It is the same set") {
+				REQUIRE(hull.size() == in.size());
+				REQUIRE(is_subset(hull, in));
+			}
+		}
+	}
+
+	GIVEN("10 randomized tests with sets of 100 points") {
+		// the given/when structure here not matching
+		// previous tests is due to how Catch2 works with loops
+		// (ie. doesn't work)
+
+		WHEN("Convex Hull is calculated") {
+			for (int i = 0; i < 10; ++i) {
+				uniform_real_distribution<double> dist(-1000, 1000);
+				default_random_engine gen(time(NULL));
+
+				set<Point> in;
+				while (in.size() < 100) {
+					in.insert({dist(gen), dist(gen)});
+				}
+
+				vector<Point> in_vec(in.begin(), in.end());
+
+				vector<Point> hull = convex_hull(in_vec);
+
+				THEN("It is a subset of the input") {
+					REQUIRE(is_subset(in_vec, hull));
+				}
+
+				THEN("It has at least 3 points") { REQUIRE(hull.size() >= 3); }
+
+				THEN("It is convex") { REQUIRE(is_convex(hull)); }
+
+				THEN("All input points are inside the hull") {
+					REQUIRE(all_inside_polygon(in_vec, hull));
+				}
+			}
+		}
+	}
 }
 
 // ------- helper functions implementation -------
@@ -76,6 +179,7 @@ bool is_convex(vector<Point> &polygon) {
 // O(n^2) - this can be optimized to O(nlogn) using more sophisticated algorithm
 bool all_inside_polygon(vector<Point> &points, vector<Point> &polygon) {
 	// points on the boundary are considered inside
+	// the polygon should be given in counter-clockwise order
 
 	for (Point &p : points) {
 		bool is_inside = false;
