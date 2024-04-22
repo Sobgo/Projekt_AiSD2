5b76b1f3427397f21f6c218afc35f88e62badf03
mikolajjuda <mikolaj.juda@gmail.com>
2024-04-22 19:44:30 +0200

    included cstdint

--- src/bipartite_maximum_matching_lib/bipartite_maximum_matching.h
+++ src/bipartite_maximum_matching_lib/bipartite_maximum_matching.h
@@ -2,6 +2,7 @@
 #define BIPARTITE_MAXIMUM_MATCHING_H
 
 #include <vector>
+#include <cstdint>
 
 std::vector<std::pair<size_t, size_t>> bipartite_maximum_matching(const std::vector<std::pair<size_t, size_t>> &pairs);
 
