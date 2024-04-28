88308a2aefe5ce6b11359d7ce059d7cbe65f2963
Mateusz Sobkowiak <60814050+Sobgo@users.noreply.github.com>
2024-04-28 12:30:34 +0200

    Revert "Implementacja znajdowania najwiekszego skojarzenia w grafie dwudzieln…"
    This reverts commit c017a5b5ca763f0255ccc7d9d54e2f50e8248669.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
+#include "./src/test_lib.h"
 #include <iostream>
 
 using namespace std;
 
 int main() {
-	cout << "Hello, World!" << '\n';
+	cout << "Hello, World!" << endl;
+	cout << "1 + 2 = " << add(1, 2) << endl;
 
 	return 0;
 }
