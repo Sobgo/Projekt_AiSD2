b28953463f50bee50030a40fe4f429dbe978cb36
mikolajjuda <mikolaj.juda@gmail.com>
2024-05-12 16:44:26 +0200

    added more tests

--- src/sssp_plane_lib/sssp_plane.hpp
+++ src/sssp_plane_lib/sssp_plane.hpp
@@ -12,13 +12,13 @@ struct SSSP_Path {
 	std::vector<std::size_t> path;
 	double distance;
 
-    SSSP_Path(std::size_t destination, std::vector<std::size_t> path, double distance);
-    bool operator==(const SSSP_Path &other) const;
+	SSSP_Path(std::size_t destination, std::vector<std::size_t> path, double distance);
+	bool operator==(const SSSP_Path &other) const;
 };
 
 std::vector<SSSP_Path> sssp_plane(const std::vector<std::pair<double, double>> &points,
-                      const std::vector<std::pair<std::size_t, std::size_t>> &edges,
-                      std::size_t source, const std::vector<std::size_t> &destinations);
+                                  const std::vector<std::pair<std::size_t, std::size_t>> &edges,
+                                  std::size_t source, const std::vector<std::size_t> &destinations);
 
 }
 
--- test/sssp_plane_test.cpp
+++ test/sssp_plane_test.cpp
@@ -1,7 +1,13 @@
 #include <catch2/catch_test_macros.hpp>
+#include <cmath>
+#include <set>
 #include <vector>
 
 #include "../src/sssp_plane_lib/sssp_plane.hpp"
+
+bool is_valid_path(const sssp_plane::SSSP_Path &path,
+                   const std::vector<std::pair<double, double>> &points,
+                   const std::vector<std::pair<std::size_t, std::size_t>> &edges, size_t start);
 
 TEST_CASE("sssp_plane empty", "[sssp_plane]") {
 	REQUIRE_THROWS(sssp_plane::sssp_plane({}, {}, 0, {}));
@@ -11,10 +17,61 @@ TEST_CASE("sssp_plane single point", "[sssp_plane]") {
 	auto result = sssp_plane::sssp_plane({{0, 0}}, {}, 0, {0});
 	std::vector<sssp_plane::SSSP_Path> expected = {sssp_plane::SSSP_Path(0, {0}, 0)};
 	REQUIRE(result == expected);
+	REQUIRE(is_valid_path(result[0], {{0, 0}}, {}, 0));
 }
 
 TEST_CASE("sssp_plane single edge", "[sssp_plane]") {
 	auto result = sssp_plane::sssp_plane({{0, 0}, {1, 0}}, {{0, 1}}, 0, {1});
 	std::vector<sssp_plane::SSSP_Path> expected = {sssp_plane::SSSP_Path(1, {0, 1}, 1)};
 	REQUIRE(result == expected);
+	REQUIRE(is_valid_path(result[0], {{0, 0}, {1, 0}}, {{0, 1}}, 0));
+}
+
+TEST_CASE("sssp_plane simple", "[sssp_plane]") {
+	std::vector<std::pair<double, double>> points = {{0, 0}, {1, 1}, {2, 1}, {0, 1}};
+	std::vector<std::pair<std::size_t, std::size_t>> edges = {{1, 0}, {2, 1}, {3, 1}};
+	std::size_t start = 2;
+	std::vector<std::size_t> destinations = {0, 1};
+
+	auto result = sssp_plane::sssp_plane(points, edges, start, destinations);
+	std::vector<sssp_plane::SSSP_Path> expected = {
+	    sssp_plane::SSSP_Path(0, {2, 1, 0}, 1 + std::sqrt(2)), sssp_plane::SSSP_Path(1, {2, 1}, 1)};
+
+	REQUIRE(result == expected);
+	REQUIRE(is_valid_path(result[0], points, edges, start));
+}
+
+double euclidian_distance(const std::pair<double, double> &a, const std::pair<double, double> &b) {
+	double dx = a.first - b.first;
+	double dy = a.second - b.second;
+	return std::sqrt(dx * dx + dy * dy);
+}
+
+bool is_valid_path(const sssp_plane::SSSP_Path &path,
+                   const std::vector<std::pair<double, double>> &points,
+                   const std::vector<std::pair<std::size_t, std::size_t>> &edges,
+                   const size_t start) {
+	if (path.path.empty()) {
+		return false;
+	}
+	if (path.path.size() == 1 && path.distance != 0) {
+		return false;
+	}
+	if (path.path.front() != start) {
+		return false;
+	}
+	std::set<std::pair<std::size_t, std::size_t>> edges_set;
+	for (const auto &edge : edges) {
+		edges_set.insert(edge);
+	}
+
+	double distance = 0;
+	for (size_t i = 0; i < path.path.size() - 1; ++i) {
+		if (edges_set.find({path.path[i], path.path[i + 1]}) == edges_set.end()) {
+			return false;
+		}
+		distance += euclidian_distance(points[path.path[i]], points[path.path[i + 1]]);
+	}
+	if (distance != path.distance) return false;
+	return true;
 }
