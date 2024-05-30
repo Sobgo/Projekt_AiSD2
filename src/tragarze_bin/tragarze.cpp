4f9f16ff77e12519fd5e5089b678f9d36c3866c0
mikolajjuda <mikolaj.juda@gmail.com>
2024-05-30 12:30:30 +0200

    added tragarze executable

---
+++ src/tragarze_bin/tragarze.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+
+#include "../bipartite_maximum_matching_lib/bipartite_maximum_matching.hpp"
+
+using namespace std;
+
+int main() {
+	vector<pair<size_t, size_t>> input;
+	size_t a, b;
+	while (cin >> a >> b) {
+		input.push_back({a - 1, b - 1});
+	}
+	auto result = bipartite_maximum_matching::bipartite_maximum_matching(input);
+	for (auto &p : result) {
+		cout << p.first + 1 << " " << p.second + 1 << '\n';
+	}
+
+	return 0;
+}
