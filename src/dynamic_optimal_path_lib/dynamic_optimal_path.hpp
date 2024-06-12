44e25ddeef3cedadf266b5d7b706e6ae7c7c4374
Mateusz Sobkowiak <60814050+Sobgo@users.noreply.github.com>
2024-06-12 20:09:51 +0200

    format and lint

--- src/dynamic_optimal_path_lib/dynamic_optimal_path.hpp
+++ src/dynamic_optimal_path_lib/dynamic_optimal_path.hpp
@@ -1,13 +1,16 @@
 #ifndef DYNAMIC_OPTIMAL_PATH_HPP
 #define DYNAMIC_OPTIMAL_PATH_HPP
 
+#include <cstddef>
+#include <utility>
 #include <vector>
 
 namespace find_optimal_path {
 
-std::pair<std::vector<std::size_t>, int> optimalPath(const std::vector<std::size_t> &convexHullPoints,
-                                                 const std::vector<int> &brightness, int maxStops);
+std::pair<std::vector<std::size_t>, int>
+optimalPath(const std::vector<std::size_t> &convexHullPoints, const std::vector<int> &brightness,
+            int maxStops);
 
-} // namespace find_optimal_path
+}
 
 #endif
