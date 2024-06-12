30eeab5961a5564615679e6af4242f9eb536438f
Mateusz Sobkowiak <60814050+Sobgo@users.noreply.github.com>
2024-06-12 20:49:21 +0200

    more tests

--- test/data_compression_test.cpp
+++ test/data_compression_test.cpp
@@ -1,5 +1,9 @@
 #include "../src/data_compression_lib/data_compression.hpp"
 #include "catch2/catch_test_macros.hpp"
+#include <random>
+namespace compression_test {
+std::string generate_random_string(size_t length, size_t alphabet_size);
+}
 
 TEST_CASE("Huffman encoding and decoding", "[huffman]") {
 	std::string text =
@@ -20,4 +24,56 @@ TEST_CASE("Huffman encoding and decoding", "[huffman]") {
 	std::string decompressedText = data_compression::decompress(compressedText, huffmanCode);
 
 	REQUIRE(decompressedText == text);
+}
+
+TEST_CASE("Empty text compression and decompression", "[huffman]") {
+	std::string text = "";
+	auto huffmanCode = data_compression::generate_huffman_code(text);
+	auto compressedText = data_compression::compress(text, huffmanCode);
+	std::string decompressedText = data_compression::decompress(compressedText, huffmanCode);
+	REQUIRE(decompressedText == text);
+}
+
+TEST_CASE("Single character text compression and decompression", "[huffman]") {
+	std::string text = "A";
+	auto huffmanCode = data_compression::generate_huffman_code(text);
+	auto compressedText = data_compression::compress(text, huffmanCode);
+	std::string decompressedText = data_compression::decompress(compressedText, huffmanCode);
+	REQUIRE(decompressedText == text);
+}
+
+TEST_CASE("Repeated character text compression and decompression", "[huffman]") {
+	std::string text = "AAAAAABBBBC";
+	auto huffmanCode = data_compression::generate_huffman_code(text);
+	auto compressedText = data_compression::compress(text, huffmanCode);
+	std::string decompressedText = data_compression::decompress(compressedText, huffmanCode);
+	REQUIRE(decompressedText == text);
+}
+
+TEST_CASE("Randomized text compression and decompression", "[huffman]") {
+	for (size_t i = 0; i < 100; ++i) {
+		std::string text = compression_test::generate_random_string(100, i % 26 + 1);
+
+		auto huffmanCode = data_compression::generate_huffman_code(text);
+		auto compressedText = data_compression::compress(text, huffmanCode);
+		std::string decompressedText = data_compression::decompress(compressedText, huffmanCode);
+		REQUIRE(decompressedText == text);
+	}
+}
+
+namespace compression_test {
+
+std::string generate_random_string(size_t length, size_t alphabet_size) {
+	std::string s = "";
+
+	std::uniform_int_distribution<int> dis(0, alphabet_size - 1);
+	std::default_random_engine gen(time(NULL));
+
+	for (size_t i = 0; i < length; ++i) {
+		s.push_back('a' + dis(gen));
+	}
+
+	return s;
+}
+
 }
